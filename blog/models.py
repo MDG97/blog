784fac5d8ef84d93c0d62378deaa33ef6b782115
Admin <dashka-07051997@yandex.com>
2017-05-20 01:18:57 +0300

    another commit about pictures

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     published_date = models.DateTimeField(
             blank=True, null=True)
     article_image = models.ImageField(null=True, blank=True, upload_to="images/",
-                                       verbose_name='IMAGE')
+                                       verbose_name='Image')
 
     def publish(self):
         self.published_date = timezone.now()
